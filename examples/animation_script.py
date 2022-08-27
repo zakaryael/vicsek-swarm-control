a4b98cb5d663667d7583f572619d593be71dc2c7
EL KHIYATI Zakarya <zakarya.el-khiyati@inria.fr>
2022-08-27 09:16:12 +0200

    added animation script

---
+++ examples/animation_script.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from parameters import *
+from Swarm import *
+
+np.random.seed(0)
+pos = np.random.uniform(0,L,size=(2,N))
+orient = np.random.uniform(-np.pi, np.pi,size=N)
+
+
+from matplotlib.animation import FuncAnimation
+
+
+fig, ax= plt.subplots()
+x = np.linspace(0, L, 100)
+X, Y = (np.meshgrid(x,x))
+Z = potential.compute2(X, Y)        
+plt.imshow(Z, extent=[0, L, 0, L], origin='lower',
+                  cmap='viridis_r', alpha=0.5)
+plt.colorbar()
+qv = ax.quiver(pos[0], pos[1], np.cos(orient), np.sin(orient), color = 'grey', clim=[-np.pi, np.pi], headaxislength=10, headlength=9)
+ax.set_xlim(0, L)
+ax.set_ylim(0, L)
+plt.tight_layout()
+swarm = Swarm(pos, orient, v0, r0, eta, L, potential)
+
+def animate(i):
+    swarm.evol()
+    qv.set_offsets(swarm.positions.T)
+    qv.set_UVC(swarm.vx, swarm.vy)
+    return qv
+
+anim = FuncAnimation(fig,animate,np.arange(1, 3),interval=100)
+
+plt.show()
