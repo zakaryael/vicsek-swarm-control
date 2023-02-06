bea83edbea52f128e89b14f67e059420e749f45e
zakarya el khiyati <zakaria.elkhiyati@gmail.com>
2023-02-06 18:26:16 +0100

    fix bug

--- examples/script.py
+++ examples/script.py
@@ -1,16 +1,13 @@
 
-import sys
-sys.path.insert(0, '/Users/zakaryaelkhiyati/code/vicsek-swarm-control/')
-from Swarm import *
 from parameters import *
+from Swarm import *
+from potentials import *
 from tqdm import tqdm
 
 np.random.seed(0)
 swarm = Swarm(np.random.uniform(0,L,size=(2,N)), np.random.uniform(-np.pi, np.pi,size=N), v0, r0, eta, L, potential=potential)
 
-print(f"N is {N}")
+print(f"{N=}")
+
 for i in tqdm(range(Tmax)):
     swarm.evol()
-
-
-    
