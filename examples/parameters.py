05b2db97a5d892524ab2246bace7fe2391d27652
zakarya el khiyati <zakaria.elkhiyati@gmail.com>
2023-02-06 18:26:32 +0100

    update parameters

--- examples/parameters.py
+++ examples/parameters.py
@@ -1,22 +1,23 @@
 
 import os, sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) # add the parent directory to the path
 
-from Swarm import Potential, Swarm
+from potentials import *
 import numpy as np
 
 #parameters
 L = 10.0
-rho = 10.0
+rho = 2
 N = int(rho*L**2)
 
 r0 = 0.5
-deltat = 1.0
-factor =0.5
-v0 = 0.03#r0/deltat*factor
-eta = 0.2
+v0 = 0.01
+eta = 0.002
 
 loc = 3 * np.ones(2)
-potential = Potential(loc, 0.03, 1)
+gaussian_potential_params = {"alpha": 1, "A": 0.02}
+inverse_potential_params = {"b": 1, "n": 3, "A": 0.02}
+potential = GaussianPotential(loc, gaussian_potential_params)
+potential = InverseSquarePotential(loc, inverse_potential_params)
 
 Tmax = 100000
