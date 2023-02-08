0eec8c8c9dfe0706ce1333014d72eaef310f0509
zakarya el khiyati <zakaria.elkhiyati@gmail.com>
2023-02-08 15:03:12 +0100

    add target and control fields to parameters

--- examples/parameters.py
+++ examples/parameters.py
@@ -6,18 +6,20 @@ from potentials import *
 import numpy as np
 
 #parameters
-L = 10.0
-rho = 2
-N = int(rho*L**2)
+box_length = 10.0
+rho = 10
+N = int(rho*box_length**2)
 
 r0 = 0.5
 v0 = 0.01
 eta = 0.002
 
-loc = 3 * np.ones(2)
+loc_target = 3 * np.ones(2)
+loc_control = 7 * np.ones(2)
 gaussian_potential_params = {"alpha": 1, "A": 0.02}
-inverse_potential_params = {"b": 1, "n": 3, "A": 0.02}
-potential = GaussianPotential(loc, gaussian_potential_params)
-potential = InverseSquarePotential(loc, inverse_potential_params)
+inverse_potential_params = {"b": 1, "n": 1, "A": 0.02}
+target_potential = GaussianPotential(loc_target, gaussian_potential_params)
+control_potential = InverseSquarePotential(loc_control, inverse_potential_params)
+potential_fields = {"target": target_potential, "control": control_potential}
 
 Tmax = 100000
