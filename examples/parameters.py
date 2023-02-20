02de19f6d9f467c07618e818cdef44d0a47e89f3
zakarya el khiyati <zakaria.elkhiyati@gmail.com>
2023-02-20 16:15:32 +0100

    update parameters

--- examples/parameters.py
+++ examples/parameters.py
@@ -6,9 +6,9 @@ from potentials import *
 import numpy as np
 
 #parameters
-box_length = 1.0
+L = 1.0
 rho = 100.0
-N = int(rho*box_length**2)
+N = int(rho*L**2)
 
 r0 = 0.05
 v0 = 0.01
@@ -17,9 +17,13 @@ eta = 0.002
 loc_target = 0.7 * np.ones(2)
 loc_control = 0.3 * np.ones(2)
 gaussian_potential_params = {"alpha": 100, "A": 0.01}
+gaussian_potential_params2 = {"alpha": 100, "A": 0.00}
+
 inverse_potential_params = {"b": 0.1, "n": 2, "A": 0.0000}
 target_potential = GaussianPotential(loc_target, gaussian_potential_params)
 control_potential = GaussianPotential(loc_control, gaussian_potential_params)
 potential_fields = {"target": target_potential, "control": control_potential}
 
-Tmax = 100000
+Tmax = 1000
+
+target_radius=0.1
