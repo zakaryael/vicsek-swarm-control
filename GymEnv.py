142249ad850af064bd5bf54945e6ca71268b586c
zakarya el khiyati <zakaria.elkhiyati@gmail.com>
2023-02-21 16:23:17 +0100

    add render to csv file + minor fixes

--- GymEnv.py
+++ GymEnv.py
@@ -2,11 +2,12 @@ from Swarm import Swarm
 from potentials import *
 from SwarmVisualizer import *
 import copy
+import csv
 
 import gym
 import numpy as np
 from gym import spaces
-
+import os
 
 class SwarmEnv(gym.Env):
     """Custom Environment that follows gym interface"""
@@ -21,9 +22,11 @@ class SwarmEnv(gym.Env):
         eta=0.05,
         Tmax=1000,
         target_radius=0.1,
+        out_dir=None,
     ):
         super(SwarmEnv, self).__init__()
         self.g = np.random.default_rng(seed=42)  # random number generator
+        self.out_dir = out_dir
         self.target_radius = target_radius
         self.N = N
         self.L = L
@@ -34,19 +37,19 @@ class SwarmEnv(gym.Env):
         self.Tmax = Tmax
         self.iteration = 0  # iteration counter for the episode
         self.visualization = None
+        self.n_trapped = 0
         # Define action and observation space
         # start with the action space: continuous 2d vector
         self.action_space = spaces.Box(
             low=-0.01, high=0.01, shape=(2,), dtype=np.float32
         )  #
-        self.n_trapped_old = 0
         # now the observation space: continuous (3 * N + 5) vector
         lower_bounds = np.concatenate(
             (
                 np.zeros(2 * N),
                 -np.pi * np.ones(N),
                 np.zeros(2),
-                np.array([-np.inf, -np.inf]),
+                np.array([0, 0]),
                 np.zeros(1),
             )
         )
@@ -55,7 +58,7 @@ class SwarmEnv(gym.Env):
                 self.L * np.ones(2 * N),
                 np.pi * np.ones(N),
                 self.L * np.ones(2),
-                np.array([np.inf, np.inf]),
+                np.array([self.L, self.L]),
                 self.Tmax * np.ones(1),
             )
         )
@@ -74,6 +77,13 @@ class SwarmEnv(gym.Env):
         )
 
     def step(self, action):
+        # compute the number of agents in the target
+        self.n_trapped = np.sum(
+            self.swarm.in_range(
+                self.potential_fields["target"].loc, self.target_radius
+            ),
+            dtype=np.float32,
+        )
         # update the control potential field and evolve the swarm
         self.potential_fields["control"].update_location(
             self.potential_fields["control"].loc + action
@@ -112,17 +122,36 @@ class SwarmEnv(gym.Env):
         )
         return self.observation()
 
-    def render(self, mode="human", close=False):
-        # Render the environment to the screen
-        if self.visualization is None:
-            self.visualization = SwarmVisualizer(self.L)
-        return self.visualization.render(
-            self.swarm.positions,
-            self.swarm.orientations,
-            self.potential_fields,
-            self.potential_fields["target"].loc,
-            self.iteration,
-        )
+    def render(self, mode= None, close=False):
+        
+        if self.out_dir:
+            # Render the current state of the environment to a csv file
+            fname = os.path.join(self.out_dir, "swarm.csv")
+            # create the file if it doesn't exist
+            if not os.path.exists(fname):
+                #make a list of the header, consits of 2 self.N positions, self.N orientations, 2 target positions, 2 control positions, 1 time
+                header = [f"pos_{i}" for i in range(2 * self.N)] + [f"ori_{i}" for i in range(self.N)] + [f"target_{i}" for i in range(2)] + [f"control_{i}" for i in range(2)] + ["time"]
+                with open(fname, "w") as f:
+                    writer = csv.writer(f)
+                    writer.writerow(header)
+            # write the current state to the file
+            with open(fname, "a") as f:
+                writer = csv.writer(f)
+                writer.writerow(self.observation())
+
+        if mode == "plot":
+            # Render the environment to the screen
+            if self.visualization is None:
+                self.visualization = SwarmVisualizer(self.L)
+            return self.visualization.render(
+                self.swarm.positions,
+                self.swarm.orientations,
+                self.potential_fields,
+                self.potential_fields["target"].loc,
+                self.iteration,
+            )
+
+
 
     def close(self):
         pass
@@ -148,13 +177,7 @@ class SwarmEnv(gym.Env):
         returns:
             reward (float): the reward
         """
-        # compute the number of agents in the target
-        self.n_trapped = np.sum(
-            self.swarm.in_range(
-                self.potential_fields["target"].loc, self.target_radius
-            ),
-            dtype=np.float32,
-        )
+        
         # return -np.linalg.norm(self.swarm.positions - self.potential_fields["target"].loc, axis=0).mean()
         reward = -0.25
         # # add the number of agents in the target during this step
@@ -164,7 +187,7 @@ class SwarmEnv(gym.Env):
         if self.iteration == self.Tmax - 1:
             reward += self.n_trapped
 
-        self.n_trapped_old = self.n_trapped
+        
         # x_control, y_control = self.potential_fields["control"].loc
         # # check if the control potential field is inside the box
         # if x_control < 0 or x_control > self.L or y_control < 0 or y_control > self.L:
