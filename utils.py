bafe1d0502e22eb03ce03d54a6df26da116a165e
zakarya el khiyati <zakaria.elkhiyati@gmail.com>
2023-03-11 18:31:20 +0100

    update utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import pickle
 
+
 def save_gif(env, filename, timesteps=1000, fps=30):
     images = []
     obs = env.reset()
@@ -10,57 +11,43 @@ def save_gif(env, filename, timesteps=1000, fps=30):
     for i in range(timesteps):
         images.append(img)
         action, _ = model.predict(obs)
-        obs, _, _ ,_ = env.step(action)
+        obs, _, _, _ = env.step(action)
         img = env.render()
     imageio.mimsave(filename, images, fps=fps)
 
-def save_learned_gif(model, filename, fps=30):
+
+def save_learned_gif(model, env, filename, fps=30):
     # env is model.env
     images = []
-    obs = model.env.reset()
-    img = model.env.render()
-    while not model.env.buf_dones:
+    obs = env.reset()
+    img = env.render()
+    while not env.done():
         images.append(img)
         action, _ = model.predict(obs)
-        obs, _, _ ,_ = model.env.step(action)
-        img = model.env.render()
+        obs, _, _, _ = env.step(action)
+        img = env.render()
     imageio.mimsave(filename, images, fps=fps)
 
-def save_frames(model, folder):
+
+def save_frames(model, env, folder, fname="images.pkl"):
     # env is model.env
-    obs = model.env.reset()
-    img = model.env.render()
+    obs = env.reset()
+    img = env.render()
     images = []
     images.append(img)
-    
-    while not model.env.buf_dones:
+
+    while not env.done():
         action, _ = model.predict(obs)
-        obs, _, _ ,_ = model.env.step(action)
-        img = model.env.render()
+        obs, _, _, _ = env.step(action)
+        img = env.render()
         images.append(img)
-    #save the images list using pickle
-    with open(os.path.join(folder, "images.pkl"), "wb") as f:
+    # save the images list using pickle
+    with open(os.path.join(folder, fname), "wb") as f:
         pickle.dump(images, f)
 
-        
 
-
-# def make_gif_out_of_frames(folder, filename, fps=10):
-#     images = []
-#     for file in os.listdir(folder):
-#         if file.endswith(".npy"):
-#             images.append(np.load(os.path.join(folder, file)))
-#     imageio.mimsave(filename, images, fps=fps)
-
-
-def make_gif_out_of_frames(folder, filename, fps=10):
-    #load the images list using pickle
-    with open(os.path.join(folder, "images.pkl"), "rb") as f:
+def make_gif_out_of_frames(path, filename, fps=10):
+    # load the images list using pickle
+    with open(path, "rb") as f:
         images = pickle.load(f)
     imageio.mimsave(filename, images, fps=fps)
-    
-
-
-
-
-    
