e23e9ab5a6250c2673ab47232ac231dc1cfe8dcb
zakarya el khiyati <zakaria.elkhiyati@gmail.com>
2023-04-11 11:54:28 +0200

    update parameters

--- examples/parameters.py
+++ examples/parameters.py
@@ -8,7 +8,7 @@ from src.Potentials import *
 import numpy as np
 #parameters
 L = 1.0
-rho = 100.0
+rho = 10.0
 N = int(rho*L**2)
 
 r0 = 0.05
@@ -19,18 +19,18 @@ repulsion = 0
 boundary_conditions = "reflective"
 
 WALLS = [{"origin": np.array([0.4, 0.0]), "length": 0.45,  "axis": 1}, {"origin": np.array([0.4, 0.55]), "length": 0.45,  "axis": 1}, {"origin": np.array([0.0, 0.4]), "length": 0.45,  "axis": 0}, {"origin": np.array([0.55, 0.4]), "length": 0.45,  "axis": 0}]
-maze_path = os.path.join(project_dir, "data", "maze6bis.pckl")
+maze_path = os.path.join(project_dir, "data", "maze0.pckl")
 with open(maze_path, 'rb') as f:
     maze = pickle.load(f)
 
-WALLS = maze
+WALLS = None#maze
 
 loc_target = 0.7 * np.ones(2)
 loc_control = 0.3 * np.ones(2) #np.random.rand(2)
 
 # default values gaussian_potential_params = {"alpha": 100, "A": 0.01}
 
-gaussian_potential_params = {"alpha": 3000, "A": 0.1}
+gaussian_potential_params = {"alpha": 3000, "A": 0.01}
 gaussian_potential_params2 = {"alpha": 100, "A": 0.01}
 
 lj_potential = LennardJonesPotential(loc=np.array([0.7, 0.3]), params={"sigma": 0.001, "epsilon": 10})
@@ -48,8 +48,8 @@ target_potential = GaussianPotential(loc_target, gaussian_potential_params)
 control_potential = GaussianPotential(loc_control, gaussian_potential_params2)
 potential_fields = {"control": control_potential}
 
-Tmax = 10000
+Tmax = 200
 
-target_radius=0.01
+target_radius=0.04
 
 env_params = dict(N=N, L=L, v0=v0, r0=r0, eta=eta, potential_fields=potential_fields, Tmax=Tmax, target_radius=target_radius, boundary_conditions=boundary_conditions, walls=WALLS, repulsion=repulsion, target_location=loc_target, coefficient_of_restitution=1, seed=np.random.randint(0, 1000000))
